d2021d5ad0b92c0f9cff56b5f5420fab96d10e5c
Nitesh Shrestha <4751915+niteshrestha@users.noreply.github.com>
2018-01-24 15:02:02 +0545

    This shit works

---
+++ DisconnectTask/DIsconnectNotifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.System.Power;
+using Windows.UI.Notifications;
+
+namespace DisconnectTask
+{
+    public sealed class DIsconnectNotifier: IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral backgroundTaskDeferral = taskInstance.GetDeferral();
+            SendDisconnectNotification();
+            backgroundTaskDeferral.Complete();
+        }
+
+        private void SendDisconnectNotification()
+        {
+            if (PowerManager.RemainingChargePercent == 100)
+            {
+                string toastContent = "Battery 100% charged disconnect charger now.";
+                string toastPayload = $@"
+                                        <toast>
+                                            <visual>
+                                                <binding template='ToastGeneric'>
+                                                    <text>Disconnect Charger</text>
+                                                    <text>{toastContent}</text>
+                                                </binding>
+                                            </visual>
+                                        </toast>";
+                XmlDocument xmlPayload = new XmlDocument();
+                xmlPayload.LoadXml(toastPayload);
+
+                ToastNotification toastNotification = new ToastNotification(xmlPayload);
+                ToastNotificationManager.CreateToastNotifier().Show(toastNotification);
+            }
+        }
+    }
+}
---
+++ Full Charge/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Power;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Full_Charge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BatteryPercentage.Text = PowerManager.RemainingChargePercent.ToString() + "%";
+            RegisterBakgroundTask();
+        }
+
+        private const string taskName = "FullChargeDisconnect";
+        private const string taskEntryPoint = "DisconnectTask.DIsconnectNotifier";
+
+        private async void RegisterBakgroundTask()
+        {
+            var backgroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
+            if (backgroundAccessStatus == BackgroundAccessStatus.AllowedSubjectToSystemPolicy || backgroundAccessStatus == BackgroundAccessStatus.AlwaysAllowed)
+            {
+                foreach (var task in BackgroundTaskRegistration.AllTasks)
+                {
+                    if (task.Value.Name == taskName)
+                    {
+                        task.Value.Unregister(true);
+                    }
+                }
+
+                var taskBuilder = new BackgroundTaskBuilder();
+                taskBuilder.Name = taskName;
+                taskBuilder.TaskEntryPoint = taskEntryPoint;
+                taskBuilder.SetTrigger(new TimeTrigger(15, false));
+                BackgroundTaskRegistration regesteration = taskBuilder.Register();
+            }
+        }
+    }
+}
